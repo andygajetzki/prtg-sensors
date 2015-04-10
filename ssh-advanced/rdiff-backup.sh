9503dee6d61a2d20b0822c8225b335bbda463a14
andygajetzki <andy@gajetzki.com>
2015-04-10 12:59:13 -0500

    added backup size results to rdiff-backup.sh

--- ssh-advanced/rdiff-backup.sh
+++ ssh-advanced/rdiff-backup.sh
@@ -8,11 +8,13 @@ export BACKUP_ROOT RDIFF_BACKUP MAX_AGE_IN_SECONDS
 
 checkBackup() {
 
+
+  RDIFF_INFO=$($RDIFF_BACKUP --list-increment-sizes $BACKUP_ROOT/$2)
+
   # Check for stale backups
   OUT="<result>"
   OUT+="<channel>Backup Age: $2</channel>" 
-
-  TIMESTAMP=$($RDIFF_BACKUP --list-increment-sizes $BACKUP_ROOT/$2 | tail -n +3 | head -1 | cut -c 1-25)
+  TIMESTAMP=`echo "$RDIFF_INFO" | tail -n +3 | head -1 | cut -c 1-25`
   DATE_NOW=`date +"%s"`
   DATE_THEN=`date +"%s" --date="$TIMESTAMP"`
   DATE_DIFF=`expr $DATE_NOW - $DATE_THEN`
@@ -22,7 +24,19 @@ checkBackup() {
   OUT+="<CustomUnit>Seconds</CustomUnit>"
   OUT+="<ValueLookup>prtg.standardlookups.offon.stateoffok</ValueLookup>"
   OUT+="</result>"
-   
+
+  # Size of archives
+  SIZE_HUMAN=`echo "$RDIFF_INFO" | tail -n +3 | head -1 | cut -c 25-45`
+  SIZE_MB=`units -1t -o "%f" "$SIZE_HUMAN" MB`
+
+  OUT+="<result>"
+  OUT+="<channel>Size: $2</channel>"
+  OUT+="<Value>${SIZE_MB%.*}</Value>"
+  OUT+="<CustomUnit>MB</CustomUnit>"
+  OUT+="</result>"
+
+  
+
   echo $OUT
 }
 export -f checkBackup
