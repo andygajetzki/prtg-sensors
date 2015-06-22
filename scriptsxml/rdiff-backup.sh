37fcb12bcb47bf8e9646594d0ba345da85200fd9
andy gajetzki <andy.gajetzki@engineeredair.com>
2015-06-22 13:28:22 -0600

    refactored rdiff-backup to increase speed

--- scriptsxml/rdiff-backup.sh
+++ scriptsxml/rdiff-backup.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 BACKUP_ROOT="/srv/backups/fs/rdiff"       # The root ditrectory containing your backup destinations
-RDIFF_BACKUP="/usr/bin/rdiff-backup"
 MAX_AGE_IN_SECONDS=43200                  # 12 hours or 43200 seconds
 
-export BACKUP_ROOT RDIFF_BACKUP MAX_AGE_IN_SECONDS
+export BACKUP_ROOT MAX_AGE_IN_SECONDS
 
 checkBackup() {
 
-
-  RDIFF_INFO=$($RDIFF_BACKUP --list-increment-sizes $BACKUP_ROOT/$2)
 
   # Check for stale backups
   OUT="<result>"
   OUT+="<channel>Backup Age: $2</channel>" 
   TIMESTAMP=`echo "$RDIFF_INFO" | tail -n +3 | head -1 | cut -c 1-25`
   DATE_NOW=`date +"%s"`
-  DATE_THEN=`date +"%s" --date="$TIMESTAMP"`
-  DATE_DIFF=`expr $DATE_NOW - $DATE_THEN`
+  DATE_THEN=`ls -Art $1/$2/rdiff-backup-data/session* | tail -n 1 | xargs cat  |  grep EndTime | cut -d' ' -f 2 | xargs printf "%.0f"`
+  DATE_DIFF=$((DATE_NOW-DATE_THEN))
   OUT+="<Value>$DATE_DIFF</Value>"
   OUT+="<LimitMode>1</LimitMode>"
   OUT+="<LimitMaxError>$MAX_AGE_IN_SECONDS</LimitMaxError>"
@@ -26,8 +23,8 @@ checkBackup() {
   OUT+="</result>"
 
   # Size of archives
-  SIZE_HUMAN=`echo "$RDIFF_INFO" | tail -n +3 | head -1 | cut -c 25-45`
-  SIZE_MB=`units -1t -o "%f" "$SIZE_HUMAN" MB`
+  SIZE_BYTES=`ls -Art $1/$2/rdiff-backup-data/session* | tail -n 1 | xargs cat  |  grep MirrorFileSize | cut -d' ' -f 2`
+  SIZE_MB=$((SIZE_BYTES/1048576))
 
   OUT+="<result>"
   OUT+="<channel>Size: $2</channel>"
